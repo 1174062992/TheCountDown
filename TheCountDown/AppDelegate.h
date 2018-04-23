9c7317d391752351db19745620053e6e7b9106f4
xunli <xunli@cmmcc.local>
2018-04-23 09:36:25 +0800

    Initial Commit

---
+++ TheCountDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheCountDown
+//
+//  Created by xunli on 2018/4/23.
+//  Copyright © 2018年 caoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheCountDown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheCountDown
+//
+//  Created by xunli on 2018/4/23.
+//  Copyright © 2018年 caoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
